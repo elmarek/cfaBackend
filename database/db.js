a87ccbac5b7f290b9d74675227f55c9ce6d4e07f
elmarek <mareks@gmx.com>
2021-02-03 15:12:34 -0700

    Updated DB Schema

--- database/db.js
+++ database/db.js
@@ -11,7 +11,7 @@ db.once("open", function () {
   console.log("Connected to the database");
 });
 
-const commentSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   name: String,
   password: String,
   email: String,
@@ -19,19 +19,19 @@ const commentSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now },
 });
 
-const Comments = mongoose.model("Comments", commentSchema);
+const Users = mongoose.model("Users", userSchema);
 
-const testComments = new Comments({
+const testUser = new User({
   name: "User",
   password: "test",
   email: "test@test.com",
   comments: ["Comment 1", "Comment 2", "Comment 3", "Comment 4"],
 });
 
-module.exports = Comments;
+module.exports = Users;
 
 // LOAD DB BELOW
-// testComments.save(function (err) {
+// testUser.save(function (err) {
 //   if (err) return console.error(err);
 //   console.log("DB saved");
 // });
